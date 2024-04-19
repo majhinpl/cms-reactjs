fc7f5fbb0e438dfca8d089438cbc70eeb1dd532b
nepstech <majhintech@gmail.com>
2024-04-19 17:16:26 +0300

    fetch all blogs

--- src/pages/blog/Home.jsx
+++ src/pages/blog/Home.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
 import Card from "./components/card/Card";
 import BlogSearch from "./components/blogSearch/BlogSearch";
 import Pagination from "./components/pagination/Pagination";
+import { baseUrl } from "../../config";
+import axios from "axios";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  const fethBlogs = async () => {
+    const response = await axios.get(`${baseUrl}/blog`);
+
+    if (response.status === 200) {
+      setBlogs(response.data.data);
+    }
+  };
+
+  useEffect(() => {
+    fethBlogs();
+  }, []);
+
   return (
     <Layout>
       <BlogSearch />
       <div className="mx-auto container mt-12 max-w-7xl flex flex-wrap items-center justify-center gap-2">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {blogs.length > 0 &&
+          blogs.map((blog) => {
+            return <Card blog={blog} />;
+          })}
       </div>
       <Pagination />
     </Layout>
--- src/pages/blog/components/card/Card.jsx
+++ src/pages/blog/components/card/Card.jsx
@@ -3,7 +3,8 @@ import { LuSignalMedium } from "react-icons/lu";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-router-dom";
 
-const Card = () => {
+const Card = ({ blog }) => {
+  console.log(blog);
   return (
     <div className="flex flex-col w-[400px] h-[500px] m-1 shadow-lg">
       <div className="flex justify-between px-2 py-1 mb-2">
@@ -14,29 +15,31 @@ const Card = () => {
       </div>
       <div className="mb-2">
         <img
-          src="https://images.unsplash.com/photo-1573164713714-d95e436ab8d6?ixlib=rb-4.0.3&amp;auto=format&amp;fit=crop&amp;w=1469&amp;q=80"
+          src={
+            blog.imageUrl
+              ? blog.imageUrl
+              : "https://images.pexels.com/photos/20877190/pexels-photo-20877190/free-photo-of-plaza-de-espana.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          }
           className="rounded-md object-cover w-screen h-[150px] flex text-left"
           alt=""
           title="A tips to make a lot of money"
           loading="lazy"
         />
       </div>
-      <Link to="/blog/id">
+      <Link to={`/blog/${blog._id}`}>
         <div className="blogHeading">
           <h1 className="mt-6 text-lg font-semibold text-gray-900">
-            10 Tips for Crafting the Perfect UX Portfolio
+            {blog.title}
           </h1>
         </div>
       </Link>
       <div className="blogDetails items-start">
         <p className="mt-4 w-full text-sm leading-normal text-gray-600 ">
-          Learn how to showcase your design skills and stand out in a crowded
-          job market. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Ducimus, iusto?
+          {blog.description}
         </p>
       </div>
       <div className="btn">
-        <Link to="/blog/id">
+        <Link to={`/blog/${blog._id}`}>
           <button className="flex items-center shadow-md py-2">
             Read more <MdKeyboardArrowRight />
           </button>
@@ -45,7 +48,7 @@ const Card = () => {
       <div className="mt-4 flex space-x-3 ">
         <img
           className="h-10 w-10 rounded-full object-cover"
-          src="https://images.pexels.com/photos/1449667/pexels-photo-1449667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src={blog.imageUrl}
           alt="Emily Lee"
           width={10}
           height={10}
